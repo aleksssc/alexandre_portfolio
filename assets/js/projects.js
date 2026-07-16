ad96a7bb892d231733060f651ed76a239ad9258e
aleksssc <alexcardinha14@gmail.com>
2026-07-17 00:01:14 +0100

    portfolio remaster v2.0

---
+++ assets/js/projects.js
@@ -0,0 +1,74 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    new Swiper(".projectSwiper", {
+
+        effect: "coverflow",
+
+        centeredSlides: true,
+
+        initialSlide: 1,
+
+        slidesPerView: 3,
+
+        breakpoints: {
+
+            0: {
+                slidesPerView: 1.1
+            },
+
+            576: {
+                slidesPerView: 1.5
+            },
+
+            768: {
+                slidesPerView: 2
+            },
+
+            992: {
+                slidesPerView: 3
+            }
+
+        },
+
+        loop: false,
+
+        loopedSlides: 3,
+
+        loopAdditionalSlides: 3,
+
+        grabCursor: true,
+
+        watchSlidesProgress: true,
+
+
+        coverflowEffect:{
+
+        rotate: window.innerWidth < 768 ? 0 : 18,
+
+        depth: window.innerWidth < 768 ? 0 : 250,
+
+        scale: window.innerWidth < 768 ? 1 : 0.82,
+
+        slideShadows:false
+
+        },
+
+        pagination: {
+
+            el: ".swiper-pagination",
+
+            clickable: true
+
+        },
+
+        navigation: {
+
+            nextEl: ".swiper-button-next",
+
+            prevEl: ".swiper-button-prev"
+
+        }
+
+    });
+
+});
